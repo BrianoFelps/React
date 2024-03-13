0e9d6760d9e88820497215301c602c13b9b4d963
BRIAN FELIPE GONCALVES <brian.7908@aluno.pr.senac.br>
2024-03-13 17:27:47 -0300

    listafuncoes

---
+++ src/Components/Button.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+import "./Button.css";
+
+interface Props {
+    children: ReactNode;
+    onClick: () => void;
+}
+
+function Button(props: Props){
+    return(
+        <button onClick={props.onClick}>{props.children}</button>
+    )
+}
+
+export default Button
